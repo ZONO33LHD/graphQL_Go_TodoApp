64f3651a56ad54804349fc1e256dc9563ce53ce6
ZONO33LHD <shunsuk.free@gmail.com>
2024-07-21 17:39:51 +0900

    プロジェクト追加

---
+++ backend/main/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"todo/graph"
+	"log"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+const defaultPort = "8084"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type"},
+		AllowCredentials: true,
+	}))
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	// playground.Handlerは、ブラウザ上でGraphQLクエリを試すことができる"GraphQL playground"というツールを提供します。"/query"はGraphQLリクエストを送信するエンドポイントを示します。
+	r.GET("/", gin.WrapF(playground.Handler("GraphQL playground", "/query")))
+	r.POST("/query", gin.WrapH(srv))
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	r.Run(":" + port) // デフォルトでPanicが発生するので、log.Fatalは不要
+}
